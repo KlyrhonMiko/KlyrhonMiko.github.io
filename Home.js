1f17cf8bcc3db1addb32b9c5bf81880a69535138
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2023-12-01 17:01:11 +0800

    Home

--- Home.js
+++ Home.js
@@ -1,10 +1,16 @@
 var content = document.getElementById("content")
 var button = document.getElementById("button")
+var hiddenContent = document.getElementById("hiddenContent")
+var main = document.getElementById("main")
 
 button.onclick = function () {
     if (content.className == "clicked") {
         content.className = "";
+        hiddenContent.className = "";
+        main.className = "";
     } else {
         content.className = "clicked";
+        hiddenContent.className = "show";
+        main.className = "clicked";
     }
 }
