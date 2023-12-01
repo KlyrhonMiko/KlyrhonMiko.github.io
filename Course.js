7883c14c574d32419d15b0092b39195798a0fbe7
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2023-12-01 15:55:01 +0800

    changes

--- Course.js
+++ Course.js
@@ -1,30 +1,17 @@
-var content = document.getElementById("content")
 var button = document.getElementById("course")
 var button2 = document.getElementById("button")
 var computer = document.getElementById("computer")
 var hiddenContent = document.getElementById("hiddenContent")
-
-course.onclick = function() {
-    if (content.className == "left") {
-        content.className = "";
-        computer.className = "";
-        hiddenContent.className = "";
-        course.className = "";
-    } else {
-        content.className = "left";
-        computer.className= "left"
-        hiddenContent.className = "show";
-        course.className = "clicked";
-    }
-}
 
 button2.onclick = function() {
     if (content.className == "left") {
         content.className = "";
         computer.className = "";
         hiddenContent.className = "";
+        course.className = "";
     } else {
-        content.className = "left";
+        course.className = "clicked";
+        content.className   = "left";
         computer.className= "left"
         hiddenContent.className = "show";
     }
