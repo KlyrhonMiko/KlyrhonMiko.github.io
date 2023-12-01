5a3849b014a6751a7da5923bef1927dd5d8ce8c9
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2023-12-01 21:57:22 +0800

    Subject

--- Subject.js
+++ Subject.js
@@ -3,6 +3,7 @@ var button = document.getElementById("button")
 var main = document.getElementById("main")
 var move = document.getElementById("move")
 var hiddenContent = document.getElementById("hiddenContent")
+var buttonMargin = document.getElementById("buttonMargin")
 button.onclick = function () {
     if (content.className == "clicked") {
         content.className = "";
@@ -10,12 +11,13 @@ button.onclick = function () {
         button.className = "";
         hiddenContent.className = "";
         move.className = "";
+        buttonMargin.className = "";
     } else {
         content.className = "clicked";
         main.className = "clicked";
         button.className = "clicked";
-        button.className = "clicked";
         hiddenContent.className = "show";
         move.className = "clicked";
+        buttonMargin.className = "clicked";
     }
 }
