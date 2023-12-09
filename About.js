d3b30d0655f77b4726ccafdca96165ce0dcc1907
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2023-12-09 21:51:29 +0800

    About

--- About.js
+++ About.js
@@ -1,10 +1,31 @@
+
 var content = document.getElementById("contentHover")
+var mainContent = document.getElementById("content")
 var main = document.getElementById("main")
+var hide = document.getElementById("hiddenContent")
+var button = document.getElementById("button")
+var slideshow = document.getElementById("scroll")
+
 
 content.onclick = function() {
+
     if (main.className == "show") {
         main.className = "";
-    } else {
+        mainContent.className = "";
+        slideshow.className = "";  
+        hide.className = "";   
+    }  else {
         main.className = "show";
+        hide.className = "show";
+        mainContent.className = "";
+        slideshow.className = "";
     }
+}
+
+button.onclick = function() {
+    if (main.className == "show") {
+        mainContent.className = "secondShow";
+        hide.className = "secondShow";
+        slideshow.className = "show"; 
+    }  
 }
