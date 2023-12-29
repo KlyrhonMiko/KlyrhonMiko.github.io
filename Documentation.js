8935b370ad5150722ddf37142189abc1f3eda64e
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2023-12-29 13:30:55 +0800

    documentation

--- Documentation.js
+++ Documentation.js
@@ -1,10 +1,13 @@
 var click = document.getElementById("header")
 var content = document.getElementById("content")
+var documentation = document.getElementById("documentation")
 
 click.onclick = function() {
     if (content.className == "show") {
         content.className = "";
+        documentation.className = "";
     }   else {
         content.className = "show";
+        documentation.className = "show";
     }
 }
